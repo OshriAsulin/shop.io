c3b6eea257184affaf4d3a1d40b0aa130466c856
Your NameOshriAsulin <oshri12120@gmail.com>
2023-04-18 12:30:38 +0300

    add homescreen and productscreen and css for this screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import data from './data'
+import { Route, Routes } from 'react-router-dom'
+import HomeScreen from './pages/HomeScreen'
+import ProductScreen from './pages/ProductScreen'
 function App() {
   return (
     <>
       <header>
-        <a href='/'>Shop.io</a>
+        <Link to='/'>Shop.io</Link>
       </header>
       <div>
         <main>
-          products
-          
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
         </main>
       </div>
     </>
