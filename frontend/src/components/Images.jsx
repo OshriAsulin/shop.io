67675e10d29e7ffb14c2cb75f2dac9f957165a57
Your NameOshriAsulin <oshri12120@gmail.com>
2023-05-17 15:16:51 +0300

    change images in front

--- frontend/src/components/Images.jsx
+++ frontend/src/components/Images.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
-import img1 from "../assets/shop.io-images/black_white_suit.jpg.webp"
-import img2 from "../assets/shop.io-images/MENS_CLASSIC_BLACK_SUIT.webp"
-import img3 from "../assets/shop.io-images/NAVY_BLUE_SUIT.webp"
+// import img1 from "../assets/shop.io-images/black_white_suit.jpg.webp"
+// import img2 from "../assets/shop.io-images/MENS_CLASSIC_BLACK_SUIT.webp"
+// import img3 from "../assets/shop.io-images/NAVY_BLUE_SUIT.webp"
 
 
 const Images = () => {
@@ -17,21 +17,24 @@ const imgStyle = {
 
 const images = [
     {
-      original: 'https://picsum.photos/id/1018/1000/600/',
+      // original: 'https://picsum.photos/id/1018/1000/600/',
+      original: 'https://img.freepik.com/premium-photo/men-s-casual-outfits-man-clothing-with-blue-shirt-blue-jeans-belt-watch-sunglasses-yellow-boot-isolated-white-background-top-view_107612-106.jpg?w=2000',
       thumbnail: 'https://picsum.photos/id/1018/250/150/',
     },
     {
-      original: 'https://picsum.photos/id/1015/1000/600/',
+      original: 'https://img.freepik.com/premium-photo/men-s-clothing-set-with-oxford-shoes-watch-blue-trousers-sunglasses-office-shirt-yellow-jacket-isolated-white-background-top-view_107612-72.jpg',
+      // original: 'https://picsum.photos/id/1015/1000/600/',
       thumbnail: 'https://picsum.photos/id/1015/250/150/',
     },
     {
-      original: 'https://picsum.photos/id/1019/1000/600/',
+      original: 'https://img.freepik.com/premium-photo/men-s-casual-outfits-with-black-boot-watch-jeans-belt-wallet-sunglasses-office-shirt-blue-jacket-bracelet-tie-isolated-white-background-top-view_107612-64.jpg?size=626&ext=jpg&ga=GA1.1.183093283.1683704230&semt=ais',
+      // original: 'https://picsum.photos/id/1019/1000/600/',
       thumbnail: 'https://picsum.photos/id/1019/250/150/',
     },
   ];
     return (
     
-        <ImageGallery autoPlay items={images} showIndex={false} showPlayButton={false} showNav={false} showThumbnails={false} showFullscreenButton={false}/>
+        <ImageGallery autoPlay items={images} showIndex={false} showPlayButton={false} showNav={true} showThumbnails={false} showFullscreenButton={false}/>
         // <div><img src={img1} style={imgStyle} /></div>
         // <div><img src={img2} style={imgStyle} /></div>
         // <div><img src={img3} style={imgStyle} /></div>
