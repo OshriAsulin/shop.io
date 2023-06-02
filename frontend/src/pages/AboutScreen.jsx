b822b98435a655f9ca71dff4f7e8a94768f19958
Oshri Asulin <oshri12120@gmail.com>
2023-06-02 02:00:46 +0200

    add p to about page

--- frontend/src/pages/AboutScreen.jsx
+++ frontend/src/pages/AboutScreen.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import ImagesSlides from '../components/ImagesSlides'
-import Images from '../components/Images'
-import Categories from '../components/Categories'
-
+import React from "react";
+import ImagesSlides from "../components/ImagesSlides";
+import Images from "../components/Images";
+import Categories from "../components/Categories";
 
 const AboutScreen = () => {
- 
-
- 
   return (
     <>
-          <div>
-            <h3>About This Website</h3>
-          </div>
+      <div>
+        <div class="about-page">
+          <h1>Welcome to Shop.io website!</h1>
+          <p>
+            We are a passionate team dedicated to bringing you the best online
+            shopping experience. With a focus on quality, convenience, and
+            customer satisfaction, we strive to offer a wide range of products
+            that cater to your diverse needs.
+          </p>
+          <p>
+            Whether you're searching for trendy fashion pieces, innovative
+            gadgets, or unique home decor, our curated selection ensures you'll
+            find something that resonates with your personal style.
+          </p>
+          <p>
+            Our commitment to excellence extends beyond our product offerings.
+            We pride ourselves on delivering exceptional customer service,
+            secure transactions, and fast shipping to ensure your complete
+            satisfaction. Join us on this exciting journey as we redefine the
+            way you shop online!
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutScreen
+export default AboutScreen;
